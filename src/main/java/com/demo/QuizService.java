3db1caee0522135cd56f82575f68484e3540079e
PraewoB <c.kunapakornkarn@hotmail.com>
2018-10-02 14:31:24 +0700

    bow

---
+++ src/main/java/com/demo/QuizService.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.demo.QuizServiceInterface")
+public class QuizService {
+	@WebMethod
+	public String toDay() {
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		String result = dateFormat.format(date);
+		return result;
+	}
+	
+	@WebMethod
+	public String getMonth(@WebParam(name = "month_digit") int num) {
+		String[] monList = {"","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		return monList[num];
+	}
+}
---
+++ src/main/java/com/demo/QuizServiceInterface.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+public interface QuizServiceInterface {
+	@WebMethod
+	public String toDay();
+	public String getMonth(@WebParam(name = "month_digit") int num);
+}
