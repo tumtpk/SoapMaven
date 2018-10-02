be1a4ce66ec09c31f16d3b6f161715b286641c00
Windows10 <kimweir9@gmail.com>
2018-10-02 15:25:24 +0700

    commit

--- src/main/java/com/demo/Today.java
+++ src/main/java/com/demo/Today.java
@@ -6,14 +6,19 @@ import javax.jws.WebService;
 @WebService (endpointInterface = "com.demo.TodayInterface") 
 public class Today {
 	@WebMethod
-	public String getMonth(@WebParam(name = "months")String months)				  
-	{	
-	    return months;
-	}
+	public String toDay(@WebParam(name = "date")String date) {
+		Scanner reader  = new Scanner(System.in);
+		int date;
+		return date ['02/10/2018 13:06:20'];
+}
 	
 	@WebMethod
-	public String toDay(@WebParam(name = "date")String date) {
-		
-		return date;
+	public int getMonth(@WebParam(name = "months")String months)				  
+	{	
+		int months1;
+		while ( months1 <= 12 ) {
+			months1 = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		}
+	    return months1;
 	}			
 }
