ca1b4b84729703ad1d04237377de6f8b916b34b7
NEXT Speed <fill9029f@gmail.com>
2018-10-02 15:13:50 +0700

    test3

---
+++ src/main/java/com/demo/QuizService.java
@@ -0,0 +1,45 @@
+package com.demo;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.swing.text.DateFormatter;
+
+@WebService(endpointInterface="com.demo.QuizServiceInterface")
+
+public class QuizService {
+
+	@WebMethod
+	public Date toDay() {
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date(0);
+		return date;
+		
+	}
+	@WebMethod
+	public String getMonth(@WebParam(name="num")int num) {
+		
+		 switch( num ) {
+         case 1 : return "January"; 
+         case 2 : return "February" ; 
+         case 3 : return  "March" ; 
+         case 4 : return "April"; 
+         case 5 : return  "May"; 
+         case 6 : return "June"; 
+         case 7 : return "July"; 
+         case 8 : return "August"; 
+         case 9 : return "September"; 
+         case 10: return "October"; 
+         case 11: return "November"; 
+         case 12 : return "December"; 
+         default:
+             return "hello world";
+     }
+		
+	}
+}
---
+++ src/main/java/com/demo/QuizServiceInterface.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+
+public interface  QuizServiceInterface {
+
+	@WebMethod
+	public String toDay();
+	
+	@WebMethod
+	public String getMonth(@WebParam(name="num") int num) ;
+	
+}
