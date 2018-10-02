7aa4c67e9aba4fb87f0a1fb5d0e82f6f1d188029
DESKTOP-QMEFAPR\Dell <supawadee.phopan@gmail.com>
2018-10-02 14:57:00 +0700

    test

--- src/main/java/com/demo/toDay.java
+++ src/main/java/com/demo/toDay.java
@@ -14,16 +14,9 @@ import javax.jws.WebService;
 	public class toDay {
 	@WebMethod
 	public double calculate(@WebParam(name = "n")int n) {
-
-		
-			
 			
 			Date date = new Date(0);
-			System.out.println("Current Date : " + date.toString());
-			System.out.println("==============================");
-			
-			
-		      
+			System.out.println("Current Date : " + date.toString()); 
 			Calendar c1 = Calendar.getInstance();
 			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			String currentDate = df.format(c1.getTime());
@@ -37,12 +30,55 @@ import javax.jws.WebService;
 		    System.out.println("Minute : " + gc.get(Calendar.MINUTE));
 		    System.out.println("Second : " + gc.get(Calendar.SECOND));
 			return n;
+			
 		   
 			
 		}
-		
-		
-	}
+	@WebService(endpointInterface = "com.demo.toDay")
+	public class SwitchDemo {
+		@WebMethod
+		public double calculate(@WebParam(name = "month")int month) { {
 
+	        int month = 8;
+	        String monthString;
+	        switch (month) {
+	            case 1:  monthString = "January";
+	                     break;
+	            case 2:  monthString = "February";
+	                     break;
+	            case 3:  monthString = "March";
+	                     break;
+	            case 4:  monthString = "April";
+	                     break;
+	            case 5:  monthString = "May";
+	                     break;
+	            case 6:  monthString = "June";
+	                     break;
+	            case 7:  monthString = "July";
+	                     break;
+	            case 8:  monthString = "August";
+	                     break;
+	            case 9:  monthString = "September";
+	                     break;
+	            case 10: monthString = "October";
+	                     break;
+	            case 11: monthString = "November";
+	                     break;
+	            case 12: monthString = "December";
+	                     break;
+	            default: monthString = "Invalid month";
+	                     break;
+	        }
+	        System.out.println(monthString);
+	    }
+	}
+	
+	
+			
+	}
+	
+		
+		
+	
 
 
