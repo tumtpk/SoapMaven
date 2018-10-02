924c93134650312e9499f0a7eda66f0c10031eaf
arifeen008 <feenkundee@gmail.com>
2018-10-02 15:23:45 +0700

    9

--- src/main/java/com/demo/QuizServiceInterface.java
+++ src/main/java/com/demo/QuizServiceInterface.java
@@ -3,10 +3,14 @@ package com.demo;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+
+
 @WebService(endpointInterface = "com.demo.QuizService")
+
 public interface QuizServiceInterface {
 	@WebMethod
 	public String toDay();
+	
 	@WebMethod
 	public String getMonth(@WebParam (name = "number") int number);
 
