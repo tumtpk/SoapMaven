2f305f0c4f7f12a1fa6e0773f0886625150f19f9
arifeen008 <feenkundee@gmail.com>
2018-10-02 15:08:50 +0700

    test 7

--- src/main/java/com/demo/QuizService.java
+++ src/main/java/com/demo/QuizService.java
@@ -19,7 +19,7 @@ public class QuizService {
 	}
 	
 	@WebMethod
-	public int getMonth(@WebParam (name = "number") int number){
+	public String getMonth(@WebParam (name = "number") int number){
 		if(number == 1) {
 			return "January";
 		}
--- src/main/java/com/demo/QuizServiceInterface.java
+++ src/main/java/com/demo/QuizServiceInterface.java
@@ -3,11 +3,11 @@ package com.demo;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-@WebService(endpointInterface = "com.demo.QuizServiceInterface")
+@WebService(endpointInterface = "com.demo.QuizService")
 public interface QuizServiceInterface {
 	@WebMethod
 	public String toDay();
 	@WebMethod
-	public int getMonth(@WebParam (name = "number") int number);
+	public String getMonth(@WebParam (name = "number") int number);
 
 }
