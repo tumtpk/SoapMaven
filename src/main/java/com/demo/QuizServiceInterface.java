0deb25da83e4f448aedb8426f8a58c3a7b4840a6
godzilowen <owen1491@gmail.com>
2018-10-02 15:00:43 +0700

    commit

--- src/main/java/com/demo/QuizServiceInterface.java
+++ src/main/java/com/demo/QuizServiceInterface.java
@@ -5,14 +5,14 @@ import java.util.Date;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-
+import org.junit.runners.Parameterized.Parameters;
 
 @WebService
 
 public interface QuizServiceInterface {
 	
 	@WebMethod
-	public String toDay(@WebParam(name = "date") Date date);
+	public Date toDay(@WebParam(name = "date") Date date);
 	
 	@WebMethod
 	public int getMonth(@WebParam(name = "Month") int Month);
