acc6974eef04313923b0b063c668fdf4d7463bf7
SWE-PC <SWE-PC@172.16.19.210>
2018-10-02 14:14:15 +0700

    test commit

---
+++ src/main/java/com/demo/QuizService.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.demo.QuizServiceInterface")
+public class QuizService {
+	@WebMethod
+	public String datecurrent() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
+	public String getMonth(@WebParam(name = "number")int number) {
+	
+		switch (number) {
+		case 1: return "January";
+		case 2: return "February";
+		case 3: return "March";
+		case 4: return "April";
+		case 5: return "May";
+		case 6: return "June";
+		case 7: return "July";
+		case 8: return "August";
+		case 9: return "September";
+		case 10: return "October";
+		case 11: return "November";
+		case 12: return "December";
+		default: return "numbet can't getmonth";
+		
+		}
+	}
+
+}
---
+++ src/main/java/com/demo/QuizServiceInterface.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+public interface QuizServiceInterface {
+	@WebMethod
+	public String toDay();
+	
+	@WebMethod
+	public String getMonth(@WebParam(name = "number")int number);
+}
