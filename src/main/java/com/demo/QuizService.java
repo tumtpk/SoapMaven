ebd5180b6f4fb769fea462d0f70a934c1616eefb
tdedmalaew <nuttawut.c39@gmail.com>
2018-10-02 14:39:41 +0700

    round3

--- src/main/java/com/demo/QuizService.java
+++ src/main/java/com/demo/QuizService.java
@@ -48,6 +48,8 @@ public class QuizService {
          break;
 		 case 12: monthString = "December";
          break;
+		 default: monthString = "hello world";
+         break;
 		}
 		return monthString;
 	}
